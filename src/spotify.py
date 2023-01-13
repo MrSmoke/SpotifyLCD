4a3bb28192d22e9192742182ad5856912845d139
Cameron <709976+MrSmoke@users.noreply.github.com>
2023-01-14 01:10:36 +1100

    update spotify client to use urequests

--- src/spotify.py
+++ src/spotify.py
@@ -1,4 +1,4 @@
-import requests
+import urequests as requests
 from models import CurrentTrack, ApiError
 
 SPOTIFY_API = "https://api.spotify.com/v1/"
@@ -11,21 +11,22 @@ headers = {
 
 def get_currently_playing() -> CurrentTrack | ApiError | None:
     try:
-        res = requests.get(url=SPOTIFY_API + "me/player/currently-playing",
-                           headers=headers)
-    except:
+        response = requests.get(url=SPOTIFY_API + "me/player/currently-playing",
+                          headers=headers)
+    except Exception as ex:
+        print(f"Failed to access Spotify: {ex=}, {type(ex)=}")
         return ApiError(0, "Failed to access Spotify")
 
     # If we have no content, then we have no track playing, so return nothing
-    if res.status_code == 204:
+    if response.status_code == 204:
         return None
 
-    json_data = res.json()
+    json_data = response.json()
     error = json_data.get("error")
 
     # Check if we have an error and if we do, return that
     if error is not None:
-        return ApiError(res.status_code, error.get("message", ""))
+        return ApiError(response.status_code, error.get("message", ""))
 
     # Extract the track data and return a CurrentTrack object
     track = json_data["item"]
