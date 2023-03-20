00374002875cb29026579166b832d5788f50f267
Cameron <709976+MrSmoke@users.noreply.github.com>
2023-03-20 22:47:45 +1100

    Async the main thread

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from config import Config
 from time import sleep
 import network
 import machine
+import uasyncio as asyncio
 
 from spotify import Spotify
 from display import Display
@@ -21,7 +22,6 @@ display = Display(columns=int(config.get('lcd.columns')),
 spotify = Spotify(client_id=config.get('spotify.clientId'),
                   client_secret=config.get('spotify.clientSecret'))
 
-
 def get_display_text(currently_playing) -> list[str]:
 
     if type(currently_playing) is ApiError:
@@ -33,7 +33,7 @@ def get_display_text(currently_playing) -> list[str]:
     return ["", ""]
 
 
-def connect():
+async def connect():
     # Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
     count = 0
@@ -51,35 +51,48 @@ def connect():
     while not wlan.isconnected():
         count += 1
         print(f'[{count}] Waiting for connection...')
-        sleep(1)
+        await asyncio.sleep(1)
 
     print(wlan.ifconfig())
 
+# The main function
+async def main():
+    display.on()
+    display.print_lines(["Connecting to Wifi..."])
 
-print("Starting...")
+    print("Starting...")
+
+    # Connect to the WiFi
+    try:
+        await connect()
+    except KeyboardInterrupt:
+        print("stopped.")
+        exit()
+
+    # Turn on LED to say we are ready
+    led = machine.Pin("LED", machine.Pin.OUT)
+    led.on()
+
+    display.print_lines(["Connected!"])
+
+    # Start the main loop
+    while True:
+        current_track = spotify.get_currently_playing()
+
+        if current_track is None:
+            # Turn off display as there is nothing playing
+            display.off()
+
+        else:
+            # Ensure the display is on
+            display.on()
+
+            text_lines = get_display_text(current_track)
+            display.print_lines(text_lines)
+
+        await asyncio.sleep(10)
 
 try:
-    connect()
-except KeyboardInterrupt:
-    print("stopped.")
-    exit()
-
-# Turn on LED to say we are ready
-led = machine.Pin("LED", machine.Pin.OUT)
-led.on()
-
-while True:
-    current_track = spotify.get_currently_playing()
-
-    if current_track is None:
-        # Turn off display as there is nothing playing
-        display.off()
-
-    else:
-        # Ensure the display is on
-        display.on()
-
-        text_lines = get_display_text(current_track)
-        display.print_lines(text_lines)
-
-    sleep(10)
+    asyncio.run(main())
+finally:
+    asyncio.new_event_loop()
